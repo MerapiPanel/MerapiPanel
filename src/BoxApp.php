8154ab19c3606b6ecfba07fcb9c1eefc478b9c2f
il4mb <durianbohong@gmail.com>
2023-09-17 21:23:30 +0700

    Changes to be committed: 	modified:   src/BoxApp.php 	deleted:    src/Module/Api/Service.php 	deleted:    src/Module/Api/controller/guest.php 	modified:   src/config/app.yml

--- src/BoxApp.php
+++ src/BoxApp.php
@@ -41,113 +41,35 @@ class BoxApp extends Box
     final public function __call($name, $arguments)
     {
 
-        if (class_exists($name)) {
+        $name = str_replace(__NAMESPACE__, "", $name);
+        $name = strtolower(str_replace("\\", "_", str_replace("/", "_", $name)));
+        $name = ltrim(str_replace(strtolower(trim(__NAMESPACE__)) . "_", "", $name), "_");
 
-            $name = str_replace(__NAMESPACE__, "", $name);
-            $name = strtolower(str_replace("\\", "_", $name));
-            $name = str_replace(strtolower(trim(__NAMESPACE__)) . "_", "", $name);
-        }
+        $theClass = __NAMESPACE__ . "\\" . implode("\\", array_map("ucfirst", explode("_", $name)));
+        $className = $theClass;
 
-
-        $className = __NAMESPACE__;
-        $nested = &$this->stack;
-        $segments = explode("_", $name);
-
-        foreach ($segments as $x => $key) {
-
-            if (strpos($className, "\\" . ucfirst($key))) {
-
-                unset($segments[$x]);
-            } else {
-
-                $segments = array_values($segments);
-                break;
-            }
-        }
-
-        $segments = array_filter($segments);
-
-        foreach ($segments as $x => $key) {
-
-            if (!isset($nested[$key])) {
-                $nested[$key] = [];
-            }
-
-            if ($x < count($segments) - 1) {
-                // Ensure that "entity" key is set if the next segment is not the last one
-                if (is_object($nested[$key])) {
-                    $nested[$key] = ["service" => $nested[$key]];
-                }
-            }
-
-            // Append the current segment to $className, ensuring there's no duplication
-            if (strpos($className, "\\" . ucfirst($key)) === false) {
-                $className .= "\\" . ucfirst($key);
-            }
-
-            $nested = &$nested[$key];
-        }
-
-
-        // before construction
-        if (is_object($nested)) {
-            return $nested;
-        }
-        if (is_array($nested) && isset($nested["service"]) && is_object($nested["service"])) {
-
-            return $nested["service"];
-        }
-
-
-        if (!class_exists($className)) {
+        if(!class_exists($className)) {
 
             $className .= "\\Service";
-
-            if (class_exists($className) && is_array($nested)) {
-
-                $nested["service"] = null;
-                $nested = &$nested["service"];
-            }
         }
 
-
-        if (!class_exists($className)) {
-
-            $e = new CodeException("Error: Module <b>" .  ucfirst($name) . "</b> not contains the class: <b>" . basename(ltrim($className, "\\")) . "</b>");
-
-            if (!file_exists(__DIR__ . "/Module/" . basename(ltrim($className, "\\")) . ".php")) {
-
-                $e->setMessage("Error: Module <b>" .  ucfirst($name) . "</b> not found");
-            }
-            $trace = debug_backtrace(DEBUG_BACKTRACE_PROVIDE_OBJECT, 2); // Get the call stack
-
-            if (isset($trace[0]['file'])) {
-                $e->setFile($trace[0]['file']); // Set the file from the caller
-            }
-            if (isset($trace[0]['line'])) {
-                $e->setLine($trace[0]['line']); // Set the line from the caller
-            }
-
-            throw $e;
+        $instance = &$this->stack;
+        if(isset($instance[$theClass])) {
+           return $instance[$theClass];
         }
 
+        $instance = &$instance[$theClass];
 
         if (class_exists($className)) {
 
-            $nested = new Proxy($className, $arguments);
+            $instance = new Proxy($className, $arguments);
 
-            if (method_exists($nested, "setBox")) {
-                call_user_func([$nested, "setBox"], $this);
+            if (method_exists($instance, "setBox")) {
+                call_user_func([$instance, "setBox"], $this);
             }
         }
 
-        // after construction
-        if (is_array($nested) && (isset($nested["service"]) && is_object($nested["service"]))) {
-
-            return $nested["service"];
-        }
-
-        return $nested;
+        return $instance;
     }
 
 
