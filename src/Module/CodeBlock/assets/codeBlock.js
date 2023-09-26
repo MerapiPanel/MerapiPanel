0fac7eee0982271f8e12d7bf350bde9e7058bca7
il4mb <durianbohong@gmail.com>
2023-09-27 04:13:30 +0700

    Changes to be committed: 	modified:   src/Module/CodeBlock/assets/codeBlock.js 	modified:   src/Module/CodeBlock/assets/dist/codeBlock.bundle.js 	modified:   src/Module/Template/Contents/6512a366321b9/index.html 	modified:   src/Module/Template/assets/dist/editor.bundle.js

--- src/Module/CodeBlock/assets/codeBlock.js
+++ src/Module/CodeBlock/assets/codeBlock.js
@@ -1,37 +1,13 @@
-
+import CodeMirror from 'codemirror';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/ambiance.css';
+import 'codemirror/mode/php/php.js';
 import $ from 'jquery';
 
-const CodeBlock = (editor) => {
+const CodeBlock = function (Editor) {
 
 
-    editor.TraitManager.addType('custom-code', {
-        createInput({ trait }) {
-
-            const icon = `<svg style="transform: rotateY(180deg); fill: currentColor;" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"/></svg>`;
-
-            this.codeText = "";
-            this.handle = $(`<button>${icon}</button>`);
-            this.handle.css({
-                padding: "1rem",
-                with: "100%",
-                height: "100%",
-            });
-            return this.handle.get(0);
-        },
-
-        onEvent({ component }) {
-            const value = this.codeText || null;
-            component.addAttributes({ value });
-        },
-
-        onUpdate({ component }) {
-            const value = component.getAttributes().value || 0;
-            this.codeText = value;
-        },
-    });
-
-
-    editor.DomComponents.addType('custom-code', {
+    const customCodeType = {
 
         // Make the editor understand when to bind `custom-code-type`
         isComponent: function (el) {
@@ -42,42 +18,79 @@ const CodeBlock = (editor) => {
         model: {
             // Default properties
             defaults: {
-                CodeBlock: 'code',
                 tagName: 'customcode',
                 droppable: false, // Can't drop other elements inside
+                code: '<?php\r\necho "Hello, world!";\r\n?>',
                 traits: [{
-                    type: "checkbox",
-                    name: "show-output",
-                    label: "Show Output",
-
-                }, {
-                    type: "custom-code",
-                    name: "CodeBlock",
-                }],
-
+                    name: 'code',
+                    changeProp: 1,
+                }
+                ]
             },
-
             init() {
-                // Here we can listen global hooks with editor.on('...')
-                editor.on('component:update:CodeBlock', this.handleValueChange);
+                Editor.on('change:code', this.handleValueChange);
             },
+
             updated(property, value, prevValue) {
-
-            },
-            removed() {
-            },
-            handleValueChange() {
-                console.log('handleValueChange');
+                if (value === "selected") {
+                    this.codeEdit();
+                }
             },
 
+            codeEdit() {
+
+                if (!this.isOnWatching) {
+                    this.watching();
+                }
+
+                const content = $(`<div><textarea class="mb-3" id="ccode">${this.defaults.code}</textarea></div>`);
+                const CodeEditor = CodeMirror.fromTextArea($(content).find("#ccode").get(0), {
+                    mode: "application/x-httpd-php",
+                    lineNumbers: true,
+                    matchBrackets: true,
+                    theme: "ambiance",
+                    lineWiseCopyCut: true,
+                    autoRefresh: true,
+                    undoDepth: 200,
+                    autoBeautify: true,
+                    autoCloseTags: true,
+                    autoCloseBrackets: true,
+                    //lineWrapping: true,
+                    styleActiveLine: true,
+                    smartIndent: true, // Missing comma
+                    indentWithTabs: true, // Remove trailing comma just to be safe
+                });
+
+                CodeEditor.on("change", () => {
+                    this.defaults.code = CodeEditor.doc.getValue();
+                });
+
+                Editor.Modal.open({
+                    title: 'Custom Code',
+                    content: content[0],
+                });
+
+                CodeEditor.refresh();
+            },
+
+            watching() {
+
+                this.isOnWatching = true;
+                const ModalEl = Editor.Modal.modal.$el;
+                (new MutationObserver((mutations) => {
+                    Editor.selectRemove(this);
+                })).observe(ModalEl[0], {
+                    attributes: true
+                });
+            }
         },
 
-        view: {
-        },
-    });
+        view: {},
+    }
 
+    Editor.DomComponents.addType('custom-code', customCodeType);
 
-    const bm = editor.BlockManager;
+    const bm = Editor.BlockManager;
 
     bm.add('custom-code', {
         label: 'Custom Code',
@@ -88,8 +101,7 @@ const CodeBlock = (editor) => {
         },
         category: 'Basic',
         attributes: {
-            customCode: 'Insert h1 block',
-
+            customCode: 'Insert ...',
         },
     });
 }
